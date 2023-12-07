a57ba182100d5e6fe11b0e9d8f368a49ca1b9c5b
GfgDE <116426582+GfgDE@users.noreply.github.com>
2023-12-07 11:51:36 +0300

    Add files via upload

---
+++ sait/src/main/java/com/example/REST/Comment.java
@@ -0,0 +1,10 @@
+package com.example.REST;
+public class Comment {
+    String text;
+    User user;
+    public Comment(String text, User user) {
+        this.text=text;
+        this.user=user;
+    }
+}
+
